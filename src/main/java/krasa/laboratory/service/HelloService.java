4f8b2ff440ee6bfd890a0ce3003dedcd2edbb9d2
Vojtech Krasa <vojta.krasa@gmail.com>
2013-02-28 23:12:37 +0100

    refactoring

--- src/main/java/krasa/laboratory/service/HelloService.java
+++ src/main/java/krasa/laboratory/service/HelloService.java
@@ -7,21 +7,21 @@ import org.springframework.core.env.Environment;
 public class HelloService {
 	@Autowired
 	Environment environment;
-	
+
 	@Value("${hello}")
 	private String hello;
 	@Value("${environment}")
-	private String fromPlaceholder;
-	private String fromjavaconfig;
-	
+	private String fromPropertyResolver;
+	private String fromJavaConfig;
+
 	public String hello() {
-		System.err.println("from placeholder " + fromPlaceholder);
-		System.err.println("from environment "+ environment.getProperty("environment"));
-		System.err.println("from javaconfig "+ fromjavaconfig);
+		System.err.println("from PropertyResolver " + fromPropertyResolver);
+		System.err.println("from Environment " + environment.getProperty("environment"));
+		System.err.println("from JavaConfig " + fromJavaConfig);
 		return hello;
 	}
 
 	public void setEnvironment(String environment) {
-		this.fromjavaconfig = environment;
+		this.fromJavaConfig = environment;
 	}
 }
