7740b5eb16ac7a938cf3052fdc1c25963d26aa69
Vojtech Krasa <vojta.krasa@gmail.com>
2013-03-23 20:53:19 +0100

    jetty upgraded

--- src/test/java/krasa/laboratory/Start.java
+++ src/test/java/krasa/laboratory/Start.java
@@ -1,16 +1,11 @@
 package krasa.laboratory;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.InputStreamReader;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
 
-import org.mortbay.jetty.Connector;
-import org.mortbay.jetty.Server;
-import org.mortbay.jetty.nio.SelectChannelConnector;
-import org.mortbay.jetty.webapp.WebAppContext;
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.nio.SelectChannelConnector;
+import org.eclipse.jetty.webapp.WebAppContext;
 
 public class Start {
 
@@ -32,40 +27,9 @@ public class Start {
 		context.setServer(server);
 		context.setContextPath("/");
 		context.setWar(path);
-		server.addHandler(context);
+		server.setHandler(context);
 		server.start();
 		server.join();
 	}
 
-	private static class MonitorThread extends Thread {
-
-		private ServerSocket socket;
-
-		public MonitorThread() {
-			setDaemon(true);
-			setName("StopMonitor");
-			try {
-				socket = new ServerSocket(5678, 1, InetAddress.getByName("127.0.0.1"));
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-
-		@Override
-		public void run() {
-			System.out.println("*** running jetty 'stop' thread");
-			Socket accept;
-			try {
-				accept = socket.accept();
-				BufferedReader reader = new BufferedReader(new InputStreamReader(accept.getInputStream()));
-				reader.readLine();
-				System.out.println("*** stopping jetty embedded server");
-				server.stop();
-				accept.close();
-				socket.close();
-			} catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		}
-	}
 }
